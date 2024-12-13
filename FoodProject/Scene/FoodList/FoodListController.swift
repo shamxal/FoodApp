8ec9f99951fa29801276d9e4c5d9b49f83b8ee5a
Shamkhal Guliyev <shamxal.guliyev91@gmail.com>
2024-12-13 19:58:08 +0400

    food list screen
    - controller and view model added
    - json file created

---
+++ FoodProject/Scene/FoodList/FoodListController.swift
@@ -0,0 +1,29 @@
+//
+//  FoodListController.swift
+//  FoodProject
+//
+//  Created by Samxal Quliyev  on 13.12.24.
+//
+
+import UIKit
+
+class FoodListController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FoodProject/Scene/FoodList/FoodListViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FoodListViewModel.swift
+//  FoodProject
+//
+//  Created by Samxal Quliyev  on 13.12.24.
+//
+
+import Foundation
+
+class FoodListViewModel {
+    
+}

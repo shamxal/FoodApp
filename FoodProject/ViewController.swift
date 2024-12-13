521e29710b41cd7fb6af3911eddf7b82a6795c9d
Shamkhal Guliyev <shamxal.guliyev91@gmail.com>
2024-12-13 19:53:50 +0400

    project created
    - storyboard added
    - appdelegate & scene delegate

---
+++ FoodProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodProject
+//
+//  Created by Samxal Quliyev  on 13.12.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
